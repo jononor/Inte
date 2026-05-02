b21f8c223acbd4b5151d4217f1eb14bd62df2e75
Johnn <John.nordstrom500@outlook.com>
2026-05-02 19:15:23 +0200

    start

--- TestProject/src/test/java/GridMapTest.java
+++ TestProject/src/test/java/GridMapTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.example.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
