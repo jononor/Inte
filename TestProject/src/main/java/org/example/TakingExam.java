a1025150bb2d1cf0cd1be29aba2782b879f63f3e
jononor <john.nordstrom500@outlook.com>
2026-05-09 12:01:03 +0900

    eds

--- TestProject/src/main/java/org/example/TakingExam.java
+++ TestProject/src/main/java/org/example/TakingExam.java
@@ -39,108 +39,6 @@ public class TakingExam extends State{
         choices.add("CourseBook 10");
     }
 
-    /**
-     * metoden har ingen betydelse utöver att testa klassen
-     */
-    public int setCollectedBooksAndLeaveExam(int number) {
-        courseBooksAndLeaveExam = number;
-        return courseBooksAndLeaveExam;
-    }
-
-    /**
-     * metoden har ingen betydelse utöver att testa klassen
-     */
-    public int getQuestionsAnswered() {
-        return questionsAnswered;
-    }
-
-    public void increaseQuestionsAnswered() {
-        questionsAnswered++;
-    }
-
-    public List<String> getChoices(List<String> playerOptions) {
-        Question currentQuestion = table[0].getQuestion(questionsAnswered);
-        System.out.println(currentQuestion.getQuestionWording());
-
-        for (int foundCourseBooks = 0; foundCourseBooks < courseBooksAndLeaveExam; foundCourseBooks++) {
-            playerOptions.add(choices.get(foundCourseBooks));
-        }
-        return playerOptions;
-    }
-
-
-    public StateMachine.States getNextState(int result) {
-        StateMachine.States nextState;
-        result = result - 1;
-        Question currentQuestion = table[0].getQuestion(questionsAnswered);
-        //Question currentQuestion = questions.get(questionsAnswered);
-        if(questionsAnswered == QUESTIONS_TO_BEAT_GAME) {
-            System.out.println("Congratulations! You have completed the exam!");
-            nextState = StateMachine.States.valueOf("COMPLETING_GAME");
-
-        } else if (result  == currentQuestion.getCorrectAnswer()) {
-            System.out.println("Correct, next question");
-            increaseQuestionsAnswered();
-            nextState = StateMachine.States.valueOf("TAKING_EXAM");
-
-        } else if (result == 0) {
-            System.out.println("bye bye");
-            nextState = StateMachine.States.valueOf("HUBWORLD");
-
-        } else {
-            System.out.println("Wrong answer!");
-            nextState = StateMachine.States.valueOf("TAKING_EXAM");
-        }
-        return nextState;
-    }
-
-    private void fillQuestionList() {
-        Question question1 = new Question("First question: What is a Integer", 1);
-        table[0].addData(question1);
-        Question question2 = new Question("Third question: Second question: What is a float", 2);
-        table[0].addData(question2);
-        Question question3 = new Question("Forth question: What is a beaver", 3);
-        table[0].addData(question3);
-        Question question4 = new Question("Fifth question: What is a largest mountain", 4);
-        table[0].addData(question4);
-        Question question5 = new Question("Sixth question: What is the tiniest animal", 5);
-        table[0].addData(question5);
-        Question question6 = new Question("Seventh question: What is the biggest building", 6);
-        table[0].addData(question6);
-        Question question7 = new Question("Eight question: What is the hardest language", 7);
-        table[0].addData(question7);
-        Question question8 = new Question("Ninth question: How do you declare a array", 8);
-        table[0].addData(question8);
-        Question question9 = new Question("Tenth What is the most popular language", 9);
-        table[0].addData(question9);
-        Question question10 = new Question("Last question: What is the biggest city language", 10);
-        table[0].addData(question10);
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (this == obj) {
-            return true;
-        }
-        TakingExam other = (TakingExam) obj;
-        for (int index = 0; index < table.length; index++) {
-            if (!table[index].equals(other.table[index])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return PRIM_NUMBER_FIVE * PRIM_NUMBER_THREE;
-    }
-
-
     public static class LinkedList {
         private static int count = 0;
         private Node head;
@@ -306,5 +204,104 @@ public class TakingExam extends State{
         private int toPositive(int num) {
             return num * NEGATIVE_ONE;
         }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        TakingExam other = (TakingExam) obj;
+        for (int index = 0; index < table.length; index++) {
+            if (!table[index].equals(other.table[index])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return PRIM_NUMBER_FIVE * PRIM_NUMBER_THREE;
+    }
+
+    public void increaseQuestionsAnswered() {
+        questionsAnswered++;
+    }
+
+    public List<String> getChoices(List<String> playerOptions) {
+        Question currentQuestion = table[0].getQuestion(questionsAnswered);
+        System.out.println(currentQuestion.getQuestionWording());
+
+        for (int foundCourseBooks = 0; foundCourseBooks < courseBooksAndLeaveExam; foundCourseBooks++) {
+            playerOptions.add(choices.get(foundCourseBooks));
+        }
+        return playerOptions;
+    }
+
+    public StateMachine.States getNextState(int result) {
+        StateMachine.States nextState;
+        result = result - 1;
+        Question currentQuestion = table[0].getQuestion(questionsAnswered);
+        //Question currentQuestion = questions.get(questionsAnswered);
+        if(questionsAnswered == QUESTIONS_TO_BEAT_GAME) {
+            System.out.println("Congratulations! You have completed the exam!");
+            nextState = StateMachine.States.valueOf("COMPLETING_GAME");
+
+        } else if (result  == currentQuestion.getCorrectAnswer()) {
+            System.out.println("Correct, next question");
+            increaseQuestionsAnswered();
+            nextState = StateMachine.States.valueOf("TAKING_EXAM");
+
+        } else if (result == 0) {
+            System.out.println("bye bye");
+            nextState = StateMachine.States.valueOf("HUBWORLD");
+
+        } else {
+            System.out.println("Wrong answer!");
+            nextState = StateMachine.States.valueOf("TAKING_EXAM");
+        }
+        return nextState;
+    }
+
+    /**
+     * metoden har ingen betydelse utöver att testa klassen
+     */
+    public int setCollectedBooksAndLeaveExam(int number) {
+        courseBooksAndLeaveExam = number;
+        return courseBooksAndLeaveExam;
+    }
+
+    /**
+     * metoden har ingen betydelse utöver att testa klassen
+     */
+    public int getQuestionsAnswered() {
+        return questionsAnswered;
+    }
+
+    private void fillQuestionList() {
+        Question question1 = new Question("First question: What is a Integer", 1);
+        table[0].addData(question1);
+        Question question2 = new Question("Third question: Second question: What is a float", 2);
+        table[0].addData(question2);
+        Question question3 = new Question("Forth question: What is a beaver", 3);
+        table[0].addData(question3);
+        Question question4 = new Question("Fifth question: What is a largest mountain", 4);
+        table[0].addData(question4);
+        Question question5 = new Question("Sixth question: What is the tiniest animal", 5);
+        table[0].addData(question5);
+        Question question6 = new Question("Seventh question: What is the biggest building", 6);
+        table[0].addData(question6);
+        Question question7 = new Question("Eight question: What is the hardest language", 7);
+        table[0].addData(question7);
+        Question question8 = new Question("Ninth question: How do you declare a array", 8);
+        table[0].addData(question8);
+        Question question9 = new Question("Tenth What is the most popular language", 9);
+        table[0].addData(question9);
+        Question question10 = new Question("Last question: What is the biggest city language", 10);
+        table[0].addData(question10);
     }
 }
