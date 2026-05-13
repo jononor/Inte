fdb7747d14b928b3f7cf787fee0e9422e1d03162
jononor <john.nordstrom500@outlook.com>
2026-05-13 21:09:39 +0900

    eds

--- TestProject/src/main/java/org/example/TakingExam.java
+++ TestProject/src/main/java/org/example/TakingExam.java
@@ -115,6 +115,9 @@ public class TakingExam extends State{
         public void increaseListSize() {
             size++;
         }
+        public LinkedList getTable() {
+            return table[size];
+        }
 
         public void addData(Question newQuestion) {
             isQuestionWordingIllegalArgument(newQuestion.getQuestionWording());
