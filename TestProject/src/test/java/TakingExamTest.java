57423c06e5b0f07833cd966f9d1642cddac3f2ea
jononor <john.nordstrom500@outlook.com>
2026-06-12 17:59:56 +0900

    remove  uneccessary comments

--- TestProject/src/test/java/TakingExamTest.java
+++ TestProject/src/test/java/TakingExamTest.java
@@ -2,11 +2,11 @@ package test.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.example.*;
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.print.Book;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
